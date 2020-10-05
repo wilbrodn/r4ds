238c2485356d9f44be823cfa220035e3ea1bc48d
wilbrodn <wilbrodn@users.noreply.github.com>
2020-10-05 12:47:40 +0300

    Create 1_run_this_before_index_rmd.r

---
+++ 1_run_this_before_index_rmd.r
@@ -0,0 +1,8 @@
+library(tidyverse)
+try(devtools::install_github("hadley/r4ds"))
+install.packages("maps")
+install.packages("nycflights13")
+install.packages("Lahman")
+install.packages("microbenchmark")
+install.packages("pryr")
+install.packages("ggrepel")
